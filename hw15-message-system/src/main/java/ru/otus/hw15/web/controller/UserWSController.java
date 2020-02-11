6a8e688515a6ad4c9fb0480d8b04ec924b206c12
valeo.popov <valeo.popov@gmail.com>
2020-02-11 11:55:54 +0300

    method for loading user by id

--- hw15-message-system/src/main/java/ru/otus/hw15/web/controller/UserWSController.java
+++ hw15-message-system/src/main/java/ru/otus/hw15/web/controller/UserWSController.java
@@ -1,5 +1,6 @@
 package ru.otus.hw15.web.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -16,11 +17,14 @@ public class UserWSController {
         this.template = template;
     }
 
+    @MessageMapping("/getUser.{userId}")
+    public void userById(@DestinationVariable long userId) {
+        frontendService.getUserData(userId, data -> this.template.convertAndSend("/topic/user." + userId, data));
+    }
+
     @MessageMapping("/getAllUsers")
     public void allUsers() {
-        frontendService.getAllUsersData(data -> {
-            this.template.convertAndSend("/topic/allUsers", data);
-        });
+        frontendService.getAllUsersData(data -> this.template.convertAndSend("/topic/allUsers", data));
     }
 
 
