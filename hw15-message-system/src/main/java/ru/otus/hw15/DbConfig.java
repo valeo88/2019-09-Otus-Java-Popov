b9e1a6f95dba81aabc63f5f0ecc3aad6d5b515e6
valeo.popov <valeo.popov@gmail.com>
2020-02-13 08:20:20 +0300

    fix config bean injections

--- hw15-message-system/src/main/java/ru/otus/hw15/DbConfig.java
+++ hw15-message-system/src/main/java/ru/otus/hw15/DbConfig.java
@@ -1,9 +1,7 @@
 package ru.otus.hw15;
 
 import org.hibernate.SessionFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import ru.otus.hw15.api.model.AddressDataSet;
 import ru.otus.hw15.api.model.PhoneDataSet;
@@ -13,15 +11,8 @@ import ru.otus.hw15.hibernate.HibernateUtils;
 import ru.otus.hw15.web.startup.AdminUserCreator;
 
 @Configuration
-@ComponentScan
 public class DbConfig {
     private static final String HIBERNATE_CFG_XML_FILE_RESOURCE = "/WEB-INF/config/hibernate.cfg.xml";
-
-    private final ApplicationContext applicationContext;
-
-    public DbConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
 
     @Bean
     public SessionFactory sessionFactory() {
@@ -30,8 +21,7 @@ public class DbConfig {
     }
 
     @Bean(initMethod = "createDefaultAdminUser")
-    public AdminUserCreator adminUserCreator() {
-        UserService userService = applicationContext.getBean(UserService.class);
+    public AdminUserCreator adminUserCreator(UserService userService) {
         return new AdminUserCreator(userService);
     }
 }
--- hw15-message-system/src/main/java/ru/otus/hw15/MessagingConfig.java
+++ hw15-message-system/src/main/java/ru/otus/hw15/MessagingConfig.java
@@ -1,8 +1,6 @@
 package ru.otus.hw15;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import ru.otus.hw15.api.handlers.CreateUserRequestHandler;
 import ru.otus.hw15.api.handlers.GetUserDataCollectionRequestHandler;
@@ -14,16 +12,9 @@ import ru.otus.hw15.web.handlers.GetUserDataResponseHandler;
 import ru.otus.hw15.web.service.FrontendService;
 
 @Configuration
-@ComponentScan
 public class MessagingConfig {
     public static final String FRONTEND_SERVICE_CLIENT_NAME = "frontendService";
     public static final String BACKEND_SERVICE_CLIENT_NAME = "backendService";
-
-    private final ApplicationContext applicationContext;
-
-    public MessagingConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
 
     @Bean(destroyMethod = "dispose")
     public MessageSystem messageSystem() {
@@ -31,8 +22,7 @@ public class MessagingConfig {
     }
 
     @Bean
-    public MsClient backendMsClient() {
-        UserService userService = applicationContext.getBean(UserService.class);
+    public MsClient backendMsClient(UserService userService) {
         MessageSystem messageSystem = messageSystem();
 
         MsClient client = new MsClientImpl(BACKEND_SERVICE_CLIENT_NAME, messageSystem);
@@ -45,8 +35,7 @@ public class MessagingConfig {
     }
 
     @Bean
-    public MsClient frontendMsClient() {
-        FrontendService frontendService = applicationContext.getBean(FrontendService.class);
+    public MsClient frontendMsClient(FrontendService frontendService) {
         MessageSystem messageSystem = messageSystem();
 
         MsClient client = new MsClientImpl(FRONTEND_SERVICE_CLIENT_NAME, messageSystem);
